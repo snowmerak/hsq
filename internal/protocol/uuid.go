dea22dba3867511a2956acf71bffcd0c7df4f3ec
snowmerak <snowmerak@outlook.com>
2025-03-16 02:11:24 +0900

    feature: Add GenerateNumberID function for unique uint32 identifier generation

--- internal/protocol/uuid.go
+++ internal/protocol/uuid.go
@@ -1,10 +1,21 @@
 package protocol
 
 import (
+	"crypto/rand"
+	"encoding/binary"
 	"strings"
 
 	"github.com/google/uuid"
 )
+
+func GenerateNumberID() (uint32, error) {
+	buf := make([]byte, 4)
+	if _, err := rand.Read(buf); err != nil {
+		return 0, err
+	}
+
+	return binary.BigEndian.Uint32(buf), nil
+}
 
 func GenerateUUID() (string, error) {
 	id, err := uuid.NewV7()
